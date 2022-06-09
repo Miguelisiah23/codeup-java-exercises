ee272e12b6580582adeecc4b6528c8256283c65b
Miguel Romero <miguel.isiah.romero23@gmail.com>
2022-06-09 15:09:35 -0500

    finished java exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,46 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.print("Hello,");
-        System.out.print("world!");
+        System.out.println("world!");
+
+        int myFavoriteNum = 23;
+        System.out.println(myFavoriteNum);
+
+        String myString = "It could be worse...";
+        System.out.println(myString);
+
+        float myNumber = (float)3.14;
+        System.out.println(myNumber);
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) "three";
+
+//              SHORT HAND
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+        System.out.println(x);
+        System.out.println(y);
+
+
     }
 }
 
