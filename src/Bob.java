5c39ed21605d3b36c8b01b0620af8f43b041a49c
Miguel Romero <miguel.isiah.romero23@gmail.com>
2022-06-13 11:22:56 -0500

    java strings exercsie

---
+++ src/Bob.java
@@ -0,0 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Bob {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        scanner.useDelimiter("\n");
+        Boolean decision = true;
+        System.out.println("Start a conversation with Bob.");
+        while (decision) {
+            String userInput = scanner.next();
+            if (userInput.endsWith("?")) {
+                System.out.println("Bob: Sure.");
+            } else if (userInput.endsWith("!")) {
+                System.out.println("Bob: Whoa, Chill out!");
+            } else if (userInput.length() == 0) {
+                System.out.println("Bob: Fine. Be that way!");
+            } else if (userInput.equalsIgnoreCase("leave chat")) {
+                decision = false;
+            } else {
+                System.out.println("Bob: Whatever");
+            }
+        }
+    }
+}
