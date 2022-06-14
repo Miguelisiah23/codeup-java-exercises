7e4cb2a4a11e6e80364b6f4569243e505a473414
Miguel Romero <miguel.isiah.romero23@gmail.com>
2022-06-14 11:04:27 -0500

    finished java method exercsie

--- src/HighLow.java
+++ src/HighLow.java
@@ -10,27 +10,23 @@ public class HighLow {
         return oneToHundred;
     }
 
-    public static void OutPut(int num,int guesses) {
+    public static void OutPut(int num, int guesses) {
         Scanner scanner = new Scanner(System.in);
-        if(guesses == 5){
-            System.out.println("5 more tries before you lose !");
-        }
+        if (guesses == 5) System.out.println("5 more tries before you lose !");
         int input = scanner.nextInt();
-        if(guesses == 20){
+        guesses++;
+        if (guesses == 10) {
             System.out.println("You Lose!");
         } else if (input == num) {
             System.out.println("YOU GUESSED IT!\nIt took you " + guesses + " tries !");
         } else if (input > num) {
             System.out.println("LOWER!");
-            guesses++;
-            System.out.printf("Counter:%d%n",guesses);
-            OutPut(num,guesses);
-
+            System.out.printf("Counter:%d%n", guesses);
+            OutPut(num, guesses);
         } else {
             System.out.println("HIGHER!");
-            guesses++;
-            System.out.printf("Counter:%d%n",guesses);
-            OutPut(num,guesses);
+            System.out.printf("Counter:%d%n", guesses);
+            OutPut(num, guesses);
         }
 
     }
@@ -40,7 +36,7 @@ public class HighLow {
         System.out.println("Guess a number 1 to 100");
         int num = RandomNumber();
         int guesses = 0;
-        OutPut(num,guesses);
+        OutPut(num, guesses);
     }
 
     public static void main(String[] args) {
